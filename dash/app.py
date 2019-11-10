306f320ea372dbd123b85f842f67860ab7ff44b3
Letícia Sayuri Kawakami <syrkleticia@gmail.com>
2019-11-10 11:46:20 -0300

    Plotando um gráfico com dash.

---
+++ dash/app.py
@@ -0,0 +1,54 @@
+import dash
+import dash_core_components as dcc 
+import dash_html_components as html 
+
+# inicialização do Dash
+app = dash.Dash()
+
+# dicionário de cores
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+# criação do layout da aplicação
+# dicionário de estilos (style)
+# data = dados a serem plotados
+app.layout = html.Div(style = {'backgroundColor': colors['background']}, children = [
+    html.H1(
+        children = 'Hello Dash',
+        style = {
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    html.Div(children = 'Dash: A web application framework for Python.', style = {
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+    dcc.Graph(
+        id = 'Graph1',
+        figure = {
+            'data': [
+                {'x': [1, 2, 3],
+                 'y': [4, 1, 2],
+                 'type': 'bar',
+                 'name': 'SF'},
+                {'x': [1, 2, 3],
+                 'y': [2, 4, 5],
+                 'type': 'bar',
+                 'name': u'Montréal'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
